eeceae2b757d98babfb51cb29e8e17feeb4bac4c
Jakub Antonín Štigler <xstigl00@stud.fit.vut.cz>
2024-04-01 14:23:59 +0200

    Finish the task

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/sh
+
+swipl <<EOF
+consult('xstigl00.pl').
+u25_1.
+u25_2.
+u25_3.
+EOF
--- xstigl00.pl
+++ xstigl00.pl
@@ -1,12 +1,16 @@
 % Zadani c. 25:
-% Napiste program resici ukol dany predikatem u25(VIN), kde promenna VIN  
-% obsahuje prirozene cislo. Predikat prevede toto cislo na binarni cislo. 
+% Napiste program resici ukol dany predikatem u25(VIN), kde promenna VIN
+% obsahuje prirozene cislo. Predikat prevede toto cislo na binarni cislo.
 
-% Testovaci predikaty:                         			        
+% Testovaci predikaty:
 u25_1:- u25(113).					% 1110001
 u25_2:- u25(1024).                                      % 10000000000
 u25_3:- u25(1).                                         % 1
 u25_r:- write('Zadej VIN: '),read(VIN),u25(VIN).
 
 % Reseni:
-u25(VIN):-
+u25(0):- write(0), nl, !.
+u25(VIN):- to_bin(VIN), nl.
+
+to_bin(0):- true, !.
+to_bin(VIN):- M is mod(VIN, 2), D is VIN // 2, to_bin(D), write(M).
